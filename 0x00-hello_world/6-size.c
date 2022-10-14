e17d9199fbac02f0ec216144f41ed0714dd38b59
Agana-Joseph <josephagana8@gmail.com>
2022-10-14 15:40:22 -0700

    New message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,11 @@ int main(void)
 	 *
 	 * Return:zero if no error, Non-zero value  if errors.
 	 */
-	printf("Size of a char: %d byte(s)\n", sizeof(charType));
-	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-	printf("Size of long int: %ld byte(s)\n", sizeof(long integerType));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(long long integerType));
-	printf("Size of double: %ld byte(s)\n", sizeof(doubleType));
-	printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
 
