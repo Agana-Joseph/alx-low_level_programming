25f325ae35b388efceeb1ef465c0479eb7d95190
Agana-Joseph <josephagana8@gmail.com>
2022-10-14 13:07:50 -0700

    Print out sizes and type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
 	printf("Size of long int: %ld byte(s)\n", sizeof(long integerType));
-	printf("Size of long long int: %ld byte(s)\n" sizeof(long long integerType));
-	printf("Size of double: %ld byte(s)\n" sizeof(doubleType));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(long long integerType));
+	printf("Size of double: %ld byte(s)\n", sizeof(doubleType));
 	printf("Size of float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
 }
