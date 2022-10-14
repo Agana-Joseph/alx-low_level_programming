9f795275e19c8cf964a9e9eae1ba175f19bf12f0
Agana-Joseph <josephagana8@gmail.com>
2022-10-14 12:28:06 -0700

    Print size of types and variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("Size of float: %ld bytes\n", sizeof(floatType));
 	printf("Size of double: %ld bytes\n", sizeof(doubleType));
 	printf("Size of char: %ld bytes\n", sizeof (charType));
-	return(0)
+	return (0);
 }
 
