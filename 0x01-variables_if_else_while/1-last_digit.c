289b1a29da5be067db23f54c21f14fbd9715a11e
Agana-Joseph <josephagana8@gmail.com>
2022-10-17 07:41:31 -0700

    Asign a random number to a variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,37 +1,26 @@
-#include <stdlib.h>
-
-#include <time.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
 *
-* Description: print the value of n status:
-*
-*greater than, is zero and is not not less than 6.
-*
-* Return: Always O (Success)
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	int n;
+		int n;
+		int L;
+		char str[] = "Last digit of";
 
-	srand(time(0));
-	n = rand() -  RAND_MAX / 2;
-
-	/*My codes*/
-	if (n % 10 > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		L = n % 10;
+		if (L > 5)
+			printf("%s %d is %d and is greater than 5\n", str, n, L);
+		else if (L == 0)
+			printf("%s %d is %d and is 0\n", str, n, L);
+		else if (L < 6)
+			printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+		return (0);
 	}
-	else if (n % 10 == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
-	return (0);
-}
