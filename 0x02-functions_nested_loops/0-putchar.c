d4fcb86ba2a54e59bd58c5b2c78ad57f018e047f
Agana-Joseph <josephagana8@gmail.com>
2022-10-18 13:07:36 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Enrty point
+*
+*Discription: print _putchar
+*
+*Return: 0 if successfull
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
