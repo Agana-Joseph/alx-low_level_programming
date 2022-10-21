55c96be61af29aad8e70f09c82fe2613ba7d2e9c
Agana-Joseph <josephagana8@gmail.com>
2022-10-21 07:48:18 -0700

    Draw a striaght line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,24 +2,24 @@
 
 /**
 *print_line - draws a straight line in the terminal
-*@n: number of times the character _ should be printed
+*@n: input number of times to print '_'
+*Return: a straight line
 */
 
 void print_line(int n)
 {
+	int col;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i;
-
-		for (i = 1; <= n; i++)
+		for (col = 1; co <= n; col++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
-
 }
