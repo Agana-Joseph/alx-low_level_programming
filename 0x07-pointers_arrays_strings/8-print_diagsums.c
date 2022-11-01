d05e03c8e0aa569707a726999d4b6cba28d167b2
Agana-Joseph <josephagana8@gmail.com>
2022-11-01 12:14:36 -0700

    prints the sum of the two diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * print_diagsums - prints sum of #'s in diagnols of 
+ * @a: input square array
+ * @size: size of one dimension in array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int dex1, dex2;
+
+	for (dex1 = 0; a[dex1][7]; dex1++)
+	{
+		for (dex2 =0; dex2 < 8; dex2++)
+			_putchar(a[dex1][dex2]);
+
+		_putchar('\n');
+	}
+}
