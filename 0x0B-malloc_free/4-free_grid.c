66cff441b0789f4ed5a66d49b11c7bab5b34d316
Agana-Joseph <josephagana8@gmail.com>
2022-11-08 01:15:23 -0800

    Frees a 2-dimensional array of integers

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,9 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (; height >= 0; height--)
-			free(grid[height]);
-		free(grid);
-	}
+	int dex;
+
+	for (dex = o; dex < height; dex++)
+		free(grid[dex];
+				free(grid0;
 }
