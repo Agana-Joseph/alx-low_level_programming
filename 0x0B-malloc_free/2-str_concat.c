4eede007c40df72b3ac6870d1458469275d868a7
Agana-Joseph <josephagana8@gmail.com>
2022-11-08 00:30:38 -0800

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2[i] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	strout = malloc(sizeof(char) * (i + j + 1));
@@ -33,9 +33,9 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
-
 	limit = j;
 	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
+
 	return (strout);
 }
